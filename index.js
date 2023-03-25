efda234dbf7b8329ddaccbe615a13bef9bef9dfc
alaye2020 <alayengg@gmail.com>
2023-03-25 08:39:48 -0700

    blog project

--- index.js
+++ index.js
@@ -17,3 +17,58 @@
     }
    }
    )
+
+
+
+
+
+   let zaRo = 0;
+let wordIndex = 0;
+let sentences= ["WELCOME TO THE NEW CENTER", ];
+
+
+
+
+
+function startText() {
+    if (zaRo>=sentences.length) {
+        zaRo=0
+    }
+    let write = sentences[zaRo]
+
+    writeText(write) 
+}
+startText()
+
+function writeText(word) { 
+    wordType.innerHTML+= word[wordIndex]
+    wordIndex++
+   setTimeout(() => {
+    if (word[wordIndex]) {
+        writeText(word)
+    } else {
+        wordIndex=0
+        zaRo++
+        setTimeout(() => {
+            deletee()
+        }, 1000);
+    }
+    
+   }, 100);
+}
+
+function deletee() {
+    let cut = wordType.innerHTML.toString()
+    let a = cut.slice( cut,-1)
+
+    wordType.innerHTML =a
+     setTimeout(() => {
+        if (wordType.innerHTML=='') {
+            startText()
+           }
+           else{
+             deletee()
+           }
+    }, 100);
+    
+}
