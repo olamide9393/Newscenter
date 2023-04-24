28747e58c6467811eb478de248ed82eb1bc9eb56
alaye2020 <alayengg@gmail.com>
2023-04-24 14:14:55 -0700

    blog project

--- index.js
+++ index.js
@@ -24,7 +24,7 @@
 
    let zaRo = 0;
 let wordIndex = 0;
-let sentences= ["WELCOME TO THE NEW CENTER", ];
+let sentences= ["WELCOME TO THE NEW CENTER" ];
 
 
 
@@ -71,4 +71,30 @@ function deletee() {
            }
     }, 100);
     
+}
+
+
+
+
+
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
